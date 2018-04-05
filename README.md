f5db4127b03e019191e6582b1333b6955e45f8e0
enfrte@gmail.com <enfrte@gmail.com>
2018-04-05 15:18:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spelling test
+
+This is Finnish spelling proficiency test. Made with VueJS and BootStrap4. 
+
+[Screenshot](https://i.imgur.com/3c9y4dX.png)
---
+++ js/spelling.js
@@ -0,0 +1,169 @@
+var spelling = {
+	sentences: [
+		{
+			source: "Ei kaikki ole kultaa mikä kiiltää eikä kaikki ole hopeata mikä hohtaa.",
+			translation: "Not all that shines is gold, nor all that glows is silver.",
+			audio: "audio/01.mp3",
+		}, 
+		{
+			source: "Ei kannata mennä merta edemmäs kalaan.",
+			translation: "Not worth it to go further than the sea for fish.",
+			audio: "audio/17.mp3",
+		}, 
+		{
+			source: "Ei omena kauas puusta putoa.",
+			translation: "The apple does not fall far from the tree.",
+			audio: "audio/03.mp3",
+		}, 
+		{
+			source: "Ei pidä tehdä kärpäsestä härkästä.",
+			translation: "One shouldn't make a bull out of a fly.",
+			audio: "audio/04.mp3",
+		}, 
+		{
+			source: "Ei savua ilman tulta.",
+			translation: "No smoke without fire.",
+			audio: "audio/05.mp3",
+		}, 
+		{
+			source: "Ei vahinko tule kello kaulassa.",
+			translation: "An accident won't arrive with a bell on its neck.",
+			audio: "audio/06.mp3",
+		}, 
+		{
+			source: "Ei vanha koira valetta hauku.",
+			translation: "An old dog doesn't bark lies.",
+			audio: "audio/07.mp3",
+		}, 
+		{
+			source: "En pidä juuri minään.",
+			translation: "I do not recognize his achievements as almost anything.",
+			audio: "audio/08.mp3",
+		}, 
+		{
+			source: "Haukkuva koira ei pure.",
+			translation: "A barking dog does not bite.",
+			audio: "audio/09.mp3",
+		}, 
+		{
+			source: "Helposti saatu on helposti menetetty.",
+			translation: "What is acquired easily is lost easily.",
+			audio: "audio/10.mp3",
+		}, 
+		{
+			source: "Isoja kaloja kannattaa pyytää vaikkei saisikaan.",
+			translation: "Big fish are worth of fishing even if you don't catch one.",
+			audio: "audio/11.mp3",
+		}, 
+		{
+			source: "Joka menneitä muistelee, sitä tikulla silmään.",
+			translation: "A poke in the eye for those, who dwell on the past.",
+			audio: "audio/12.mp3",
+		}, 
+		{
+			source: "Joka paljon lupaa, se vähän antaa.",
+			translation: "Who promises a lot, gives a little.",
+			audio: "audio/13.mp3",
+		}, 
+		{
+			source: "Kun menee sutta pakoon, tulee karhu vastaan.",
+			translation: "When you flee from a wolf, you run into a bear.",
+			audio: "audio/14.mp3",
+		}, 
+		{
+			source: "Lukea kuin piru raamattua.",
+			translation: "Read like the devil reads the Bible.",
+			audio: "audio/15.mp3",
+		}, 
+		{
+			source: "Maassa maan tavalla.",
+			translation: "In a country according to its customs.",
+			audio: "audio/16.mp3",
+		}, 
+		{
+			source: "Millane emo, sellane tytär.",
+			translation: "Such mother, such daughter.",
+			audio: "audio/33.mp3",
+		}, 
+		{
+			source: "Minkälainen isä, sen lainen poikakin.",
+			translation: "Such father, such son.",
+			audio: "audio/18.mp3",
+		}, 
+		{
+			source: "Niin makaa, kuin petaa.",
+			translation: "One sleeps like one makes his bed.",
+			audio: "audio/19.mp3",
+		}, 
+		{
+			source: "Niin metsä vastaa kuin sinne huudetaan.",
+			translation: "The forest answers in the same way one shouts at it.",
+			audio: "audio/20.mp3",
+		}, 
+		{
+			source: "Oma apu, paras apu.",
+			translation: "One's own help [is] the best help.",
+			audio: "audio/21.mp3",
+		}, 
+		{
+			source: "Omena ei kauas puusta putoa.",
+			translation: "The apple does not fall far from the tree.",
+			audio: "audio/22.mp3",
+		}, 
+		{
+			source: "On taottava silloin kun rauta on kuuma.",
+			translation: "Iron must be forged when it is hot.",
+			audio: "audio/23.mp3",
+		}, 
+		{
+			source: "On vähäkin tyhjää parempi.",
+			translation: "Little is better than nothing.",
+			audio: "audio/24.mp3",
+		}, 
+		{
+			source: "Paha saa palkkansa.",
+			translation: "Evil will get its share(/pay).",
+			audio: "audio/25.mp3",
+		}, 
+		{
+			source: "Parempi karvas totuus kuin makea valhe.",
+			translation: "Better a bitter truth than a sweet lie.",
+			audio: "audio/26.mp3",
+		}, 
+		{
+			source: "Parempi pyy pivossa, kuin kymmenen oksalla.",
+			translation: "Better one hazel grouse in the bag, than ten on the branch.",
+			audio: "audio/27.mp3",
+		}, 
+		{
+			source: "Rakkaalla lapsella on monta nimeä.",
+			translation: "A beloved child has many names.",
+			audio: "audio/28.mp3",
+		}, 
+		{
+			source: "Sitä niittää mitä kylvää.",
+			translation: "You reap what you sow.",
+			audio: "audio/29.mp3",
+		}, 
+		{
+			source: "Suu valehtelee, silmät puhuvat totta.",
+			translation: "The mouth lies, but the eyes tell the truth.",
+			audio: "audio/30.mp3",
+		}, 
+		{
+			source: "Suutarin akka ja sepän hevonen on aina huonossa kengässä.",
+			translation: "The wife of the cobbler and the horse of the smith always have bad shoes.",
+			audio: "audio/31.mp3",
+		}, 
+		{
+			source: "Tyvestä puuhun noustaan.",
+			translation: "A tree is climbed from its base.",
+			audio: "audio/32.mp3",
+		}, 
+		{
+			source: "Älä laita kaikkia munia samaan koriin.",
+			translation: "All eggs should not be put in one basket.",
+			audio: "audio/02.mp3",
+		}, 
+	],
+};
